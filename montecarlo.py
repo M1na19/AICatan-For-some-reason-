58109000dbbf0096239a11510adb1849418a80b9
M1na19 <101211772+M1na19@users.noreply.github.com>
2023-04-26 23:20:05 +0300

    Update montecarlo.py

--- montecarlo.py
+++ montecarlo.py
@@ -8,5 +8,5 @@ class MonteCarlo_tree:
 #first_attempt:ok deci monte-carlo cu functie euristica si o limita de timp
 #game_state:my hand,players_hands,hexagon poz,asezari drumuri orase pozition, hot pozition, nr_carti dezvoltare
 #prune values that go beyond a point
-#robber randomly
+#robber and dice randomly
 #have to discover balance breadth vs depth
