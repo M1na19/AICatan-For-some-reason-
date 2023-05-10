e8640e2a155ac05c1fb26249da008a1fea87a412
M1na19 <mihaiboanta@yahoo.com>
2023-05-11 00:24:22 +0300

    upps

--- control.js
+++ control.js
@@ -1,4 +1,4 @@
 var player_turn=1
 
-//setTimeout(showUpgradable([{tile:0,piece:0}]),2000)
-setTimeout(showAvialable([{tile:0,piece:2}]),2000)
+setTimeout(showUpgradable([{tile:0,piece:0}]),2000)
+//setTimeout(showAvialable([{tile:0,piece:2}]),2000)
