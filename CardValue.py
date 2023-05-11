dcffc6bef6da34dae74bd3bf85f5c9c66f319432
Mach3tryhard <73358042+Mach3tryhard@users.noreply.github.com>
2023-05-12 00:20:13 +0300

    Add files via upload

--- CardValue.py
+++ CardValue.py
@@ -1,13 +1,13 @@
 import pickle
 
 #town city road card
-port=2
-priority="town"
+Port=[2,1]
+Priority="town"
 # 0 1 2 3
 Player=0
 # lemn=0 argila=1 fan=2 oaie=3 piatra=4 index
 CardValue=[1,1,1,1,1]
-CardNumber=[0,0,0,0,0]
+
 # tileurile cu ce fel sunt, cate orase sunt si case adunate casa=1 oras=2, ce valoare are tileul, combinare pt calcule
 class Myclass:
     def __init__(self,tile,settlement,value,combined):
@@ -23,38 +23,43 @@ Cards =[[1, 1, 2, 3, 4],
         [ 0, 1, 2, 3, 4],
         [ 0, 1, 2, 3, 4]]
 
-# CU CAT VALOREA E MAI MICA CU ATAT ESTE MAI PRETIOASA
+def Calculate_CardValue(Port,Priority,Player,CardValue,MapValue):
+    CardNumber=[0,0,0,0,0]
+    # CU CAT VALOREA E MAI MICA CU ATAT ESTE MAI PRETIOASA
 
-for i in range(len(Cards)):
-    for j in range(len(Cards[i])):
-        if i!=Player :
-            CardNumber[Cards[i][j]]+=1
-        else:
-            CardValue[Cards[i][j]]+=1
-        
-for i in range(len(MapValue.tile)):
-    MapValue.combined[MapValue.tile[i]]+=MapValue.settlement[i]*((MapValue.value[i]%7)*2.77777777778/100)
+    for i in range(len(Cards)):
+        for j in range(len(Cards[i])):
+            if i!=Player :
+                CardNumber[Cards[i][j]]+=1
+            else:
+                CardValue[Cards[i][j]]+=1
+            
+    for i in range(len(MapValue.tile)):
+        MapValue.combined[MapValue.tile[i]]+=MapValue.settlement[i]*((MapValue.value[i]%7)*2.77777777778/100)
 
-for i in range(len(CardValue)):
-    CardValue[i]*=MapValue.combined[i]
-    CardValue[i]*=CardNumber[i]
+    for i in range(len(CardValue)):
+        CardValue[i]*=MapValue.combined[i]
+        CardValue[i]*=CardNumber[i]
 
-if priority=="town" :
-    CardValue[0]/=1.5
-    CardValue[1]/=1.5
-    CardValue[2]/=1.5
-    CardValue[3]/=1.5
-if priority=="city" :
-    CardValue[2]/=1.75
-    CardValue[4]/=2
-if priority=="road" :
-    CardValue[0]/=1.5
-    CardValue[1]/=1.5
-if priority=="card" :
-    CardValue[2]/=1.5
-    CardValue[3]/=1.5
-    CardValue[4]/=1.5
+    if Priority=="town" :
+        CardValue[0]/=1.5
+        CardValue[1]/=1.5
+        CardValue[2]/=1.5
+        CardValue[3]/=1.5
+    if Priority=="city" :
+        CardValue[2]/=1.75
+        CardValue[4]/=2
+    if Priority=="road" :
+        CardValue[0]/=1.5
+        CardValue[1]/=1.5
+    if Priority=="card" :
+        CardValue[2]/=1.5
+        CardValue[3]/=1.5
+        CardValue[4]/=1.5
 
-CardValue[port]/=2
-for i in range(len(CardNumber)):
-    print(CardValue[i])
+    for i in range(len(Port)):
+        CardValue[Port[i]]/=2
+    for i in range(len(CardNumber)):
+        print(CardValue[i])
+    
+Calculate_CardValue(Port,Priority,Player,CardValue,MapValue)
