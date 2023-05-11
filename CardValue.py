c42819070d78ae427e26f5ab55f21af94df4c111
Mach3tryhard <73358042+Mach3tryhard@users.noreply.github.com>
2023-05-11 23:49:01 +0300

    Add files via upload

--- CardValue.py
+++ CardValue.py
@@ -1,22 +1,29 @@
 import pickle
 
+#town city road card
+port=0
+priority="town"
+# 0 1 2 3
 Player=0
 # lemn=0 argila=1 fan=2 oaie=3 piatra=4 index
 CardValue=[1,1,1,1,1]
 CardNumber=[0,0,0,0,0]
-
+# tileurile cu ce fel sunt, cate orase sunt si case adunate casa=1 oras=2, ce valoare are tileul, combinare pt calcule
 class Myclass:
-    tile=[0]
-    settlement=[0]
-    value=[0]
+    def __init__(self,tile=[0],settlement=[0],value=[0],combined=[0]):
+        self.tile = tile
+        self.settlement = settlement
+        self.value = value
+        self.combined= combined
 
-MapValue= Myclass()
-
+MapValue= Myclass([0,1,2,3,4,0,1,2,3],[1,1,1,1,1,1,2,2,2],[4,3,12,5,6,8,2,9,10],[0,0,0,0,0])
 
 Cards =[[1, 1, 2, 3, 4], 
         [ 0, 1, 2, 3, 4],
         [ 0, 1, 2, 3, 4],
         [ 0, 1, 2, 3, 4]]
+
+# CU CAT VALOREA E MAI MICA CU ATAT ESTE MAI PRETIOASA
 
 for i in range(len(Cards)):
     for j in range(len(Cards[i])):
@@ -25,5 +32,29 @@ for i in range(len(Cards)):
         else:
             CardValue[Cards[i][j]]+=1
         
+for i in range(len(MapValue.tile)):
+    MapValue.combined[MapValue.tile[i]]+=MapValue.settlement[i]*((MapValue.value[i]%7)*2.77777777778/100)
+
+for i in range(len(CardValue)):
+    CardValue[i]*=MapValue.combined[i]
+    CardValue[i]*=CardNumber[i]
+
+if priority=="town" :
+    CardValue[0]/=1.5
+    CardValue[1]/=1.5
+    CardValue[2]/=1.5
+    CardValue[3]/=1.5
+if priority=="city" :
+    CardValue[2]/=1.75
+    CardValue[4]/=2
+if priority=="road" :
+    CardValue[0]/=1.5
+    CardValue[1]/=1.5
+if priority=="card" :
+    CardValue[2]/=1.5
+    CardValue[3]/=1.5
+    CardValue[4]/=1.5
+
+CardValue[port]/=2
 for i in range(len(CardNumber)):
-    print(CardNumber[i])
+    print(CardValue[i])
