97b78f3654a8700fbc8932e4c6f40b8dbaa8d08c
M1na19 <mihaiboanta@yahoo.com>
2023-05-12 15:39:39 +0300

    functi pt comunicare cu server

--- control.js
+++ control.js
@@ -1,4 +1,46 @@
-var player_turn=1
-
-setTimeout(showUpgradable([{tile:0,piece:0}]),2000)
-//setTimeout(showAvialable([{tile:0,piece:2}]),2000)
+const axios = require('axios');
+function get(request,player_turn)
+{
+    axios.get('http://localhost:5000',{
+    params:{
+      'action': request, 
+      'player': player_turn
+    }
+    .then(response => {
+        return response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      })
+    })
+}
+function put(request,player_turn,information)
+{
+    axios.put('http://localhost:5000',{
+    params:{
+      'action': request, 
+      'player': player_turn,
+      'info':information
+    }
+    .catch(error => {
+        console.error(error);
+      })
+    })
+}
+function post(staringPlayer,nrPlayers)
+{
+    axios.put('http://localhost:5000',{
+    params:{
+      'sp': staringPlayer,
+      'nrP': nrPlayers
+    }
+    .then(response => {
+        return response.data;//asta e sub forma de config de tileuri
+        //config de tileuri e basicly o matrice de 19 pe 2, adica 19 tileuri care au 2 valori
+        //cele 2 valori sunt resursa si valoare
+      })
+    .catch(error => {
+        console.error(error);
+      })
+    })
+}
