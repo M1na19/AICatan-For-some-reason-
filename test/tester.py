4d93392187635379f3aeebec850380a47e877a97
M1na19 <mihaiboanta@yahoo.com>
2023-05-12 16:08:32 +0300

    start of tester

---
+++ test/tester.py
@@ -0,0 +1,12 @@
+import pickle
+import random
+generation=1
+top_value=[]
+varCoefficient=10
+for i in range(10):
+    pass
+def geneticAlgorithm(range,top):
+    proportions=[]
+    for i in range(range):
+        proportions.append()
+        
