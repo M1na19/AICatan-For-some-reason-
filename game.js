a794692cd41ad340306a4356ee4ff9beee39f0cb
Matei Sirghe <matei.sirghe@gmail.com>
2023-05-14 21:14:15 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,4 @@
 var OrdineaJucatorilor=[0,1,2,3];
-var Settlements=[];
-var Cards=[];
-var DCards=[];
-Cards.push([]);
-Settlements.push([]);
 
 function shuffle(array)
 {
@@ -21,32 +16,27 @@ function OnStartGame()
     shuffle(OrdineaJucatorilor);
     for(var i=1;i<=OrdineaJucatorilor.length;i++)
     {
-        let construction={};
-        construction.type="town";
-        construction.x=5;
-        construction.x=4;
-        Settlements[i].push(construction);
-        // ALEGI CE LOC SA PUI CASA
-        // ALEGI SA PUI DRUMUL
+        if(i==0)
+        {
+            showAvailable(get("posibile_asezari"),0);
+            showAvailable(get("posibile_drumuri"),0);
+        }
+        else
+        {
+            MakeSettlement(get("Ai_start_asezare"),"town",i);
+        }
     }
     for(var i=OrdineaJucatorilor.length;i>=1;i++)
     {
-        let construction={};
-        construction.type="town";
-        construction.x=5;
-        construction.x=4;
-        Settlements[i].push(construction);
-        let newcard={};
-        newcard.type="brick";
-        Cards[i].push(newcard);
-        newcard={};
-        newcard.type="wood";
-        Cards[i].push(newcard);
-        newcard={};
-        newcard.type="wool";
-        Cards[i].push(newcard);
-        // ALEGI CE LOC SA PUI CASA
-        // ALEGI SA PUI DRUMUL
+        if(i==0)
+        {
+            showAvailable(get("posibile_asezari"),0);
+            showAvailable(get("posibile_drumuri"),0);
+        }
+        else
+        {
+            MakeSettlement(get("Ai_start_asezare"),"town",i);
+        }
     }
 }
 
